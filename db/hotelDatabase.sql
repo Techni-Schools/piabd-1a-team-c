beb728bf50533ce40ab6403453b06212efbb64ca
Kacper Śmielak <96179878+Smieluuu@users.noreply.github.com>
2022-04-10 15:22:57 +0200

    Update

--- db/hotelDatabase.sql
+++ db/hotelDatabase.sql
@@ -1,67 +1,97 @@
 CREATE DATABASE Hotel
-GO
 
-USE Hotel
+USE [Hotel]
 GO
-
-CREATE TABLE [Address] (
-	address_id INT PRIMARY KEY, 
-	-- add IDENTITY/AUTOINCREMENT 
-	person_id INT FOREIGN KEY REFERENCES Person (person_id),
-	street  VARCHAR(50) NOT NULL,
-	city  VARCHAR(50) NOT NULL,
-	[state] VARCHAR(50) NOT NULL,
-	postal_code CHAR(5) NOT NULL,
-	country VARCHAR(50) NOT NULL,
-	-- add named CONSTRAINT
-);
 
 CREATE TABLE Person (
-	person_id INT PRIMARY KEY,
+	id INT PRIMARY KEY IDENTITY(1,1),
 	-- add IDENTITY
 	[name] VARCHAR(50) NOT NULL,
 	surname VARCHAR(50) NOT NULL,
 	phone_number INT NOT NULL,
 	email_address VARCHAR(50) NOT NULL,
-	date_of_birth DATA NOT NULL,
-	-- DATE not DATA, add CHECK for past dates only
+	date_of_birth DATE NOT NULL,
+	-- change type
 	payment_method VARCHAR(50),
+	 CONSTRAINT check_date_of_birth CHECK (date_of_birth < GETDATE()),
+	-- add CHECK for past dates only
 );
 
-CREATE TABLE Reservation (
-	reservation_id INT PRIMARY KEY,
-	-- add IDENTITY/AUTOINCREMENT
-	person_id INT FOREIGN KEY REFERENCES Person (person_id),a
-	room_id INT FOREIGN KEY REFERENCES Room (room_id),
+
+CREATE TABLE [Address] (
+	id INT IDENTITY(1,1), 
+	-- add IDENTITY
+	street  VARCHAR(50) NOT NULL,
+	city  VARCHAR(50) NOT NULL,
+	[state] VARCHAR(50) NOT NULL,
+	postal_code CHAR(5) NOT NULL,
+	country VARCHAR(50) NOT NULL,
+	person_id INT,
+	 CONSTRAINT pk_address_id PRIMARY KEY(id),
+	 CONSTRAINT fk_person_id FOREIGN KEY(person_id)
+		REFERENCES Person(id),
 	-- add named CONSTRAINT
 );
 
+
+CREATE TABLE Reservation (
+	id INT IDENTITY(1,1),
+	-- add IDENTITY
+	check_date DATE,
+	check_out_date DATE,
+	reservation_status VARCHAR(50),
+	number_of_guests SMALLINT,
+	-- change type
+	confirm_by VARCHAR(50),
+	person_id INT,
+	 CONSTRAINT pk_reservation_id PRIMARY KEY(id),
+	 CONSTRAINT fk_person_id FOREIGN KEY(person_id)
+		REFERENCES Person(id),
+	-- add named CONSTRAINT
+);
+
+
 CREATE TABLE Hotel_Personel (
-	hotel_personel_id INT PRIMARY KEY,
+	id INT IDENTITY(1,1),
 	-- add IDENTITY/AUTOINCREMENT
-	[login] VARCHAR(50),
-	-- can be null ? maybe should be UNIQUE
+	[login] VARCHAR(50) UNIQUE,
+	-- add UNIQUE
 	[password] VARCHAR(20),
 	[name] VARCHAR(50), 
 	surname VARCHAR(50),
 	[role] VARCHAR(30),
+	reservation_id INT,
+	 CONSTRAINT fk_reservation_id FOREIGN KEY(reservation_id)
+		REFERENCES Reservation(id),
 );
+
 
 CREATE TABLE Room (
-	-- where is PK ?
+	id INT IDENTITY(1,1),
 	room_number INT,
-	-- change type 
-	for_how_many_guests INT,
-	-- chane type
+	for_how_many_guests INT, 
+	-- !change type 
 	room_availability_status VARCHAR(20) DEFAULT('NO'),	
-	-- add CHECK with predefined values
+	-- !add CHECK with predefined values
+	reservation_id INT,
+	 CONSTRAINT pk_room_id PRIMARY KEY(id),
+	 CONSTRAINT fk_reservation_id FOREIGN KEY(reservation_id)
+		REFERENCES Reservation(id),
+	-- add named CONSTRAINT 
 );
 
+
 CREATE TABLE Cleaning_Service (
-	cleaning_service_id INT PRIMARY KEY,
-	room_id INT FOREIGN KEY REFERENCES Room (room_id),	
+	id INT IDENTITY(1,1),
 	cleaning_status VARCHAR(50) DEFAULT('Not Cleaned'),
-	-- add CHECK with predefined values
-	clean_up_date DATA,
-	-- DATE not DATA
+	-- !add CHECK with predefined values
+	clean_up_date DATE,
+	-- change type
+	room_id INT,
+	 CONSTRAINT pk_cleaning_service_id PRIMARY KEY(id),
+	 CONSTRAINT fk_room_id FOREIGN KEY(room_id)
+		REFERENCES Room(id),
 );
+
+
+-- komentarz z wykrzyknikiem jest informacją o rzeczy, którą należy zmienić (-- !treść)
